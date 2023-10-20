cc07e36f574b559097e1bef7f6c9e0ecbddca4b8
CharlesWGillespie <charlesgillespiew@gmail.com>
2023-10-20 19:40:25 -0400

    added function to display colors for past present and future(it does not work yet)

--- assets/script.js
+++ assets/script.js
@@ -20,15 +20,30 @@ $(function () {
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
+
+  let currentHour = $('#currentHour').dayjs().format('h a')
+  let hour = $(this).siblings('.hour').val();
+    function timeColor(){
+      if (hour === currentHour) {
+        ('div').last().addClass('present');
+      } else if (hour > currentHour){
+        ('div').last().addClass('future');
+      } else {
+        ('div').last().addClass('past');
+      }
+    }
+  }
   
-        $('#currentDay').text(dayjs().format('MM/D/YYYY'))
-        
-       
-  });
+  // TODO: Add code to get any user input that was saved in localStorage and set
+  // the values of the corresponding textarea elements. HINT: How can the id
+  // attribute of each time-block be used to do this?
+  //
   
+  localStorage.setItem(time, textVal)
+
+  // TODO: Add code to display the current date in the header of the page.
+
+,$('#currentDay').text(dayjs().format('MM/D/YYYY')) 
+)
+    
+    
